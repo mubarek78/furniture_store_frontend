97a7a092ee7a8312ebc9f363e6960552bbbe188a
mubarek78 <70493771+mubarek78@users.noreply.github.com>
2022-09-02 17:33:54 -0600

    Top nav complited

---
+++ src/components/TopNavbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const TopNavbar = () => {
+
+    return (
+        <>
+            <section id="top_navbar">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+                            <div className="top_navbar_desc">
+                                <p>Free delivery on order over $150.00</p>
+                            </div>
+                        </div>
+        
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default TopNavbar
